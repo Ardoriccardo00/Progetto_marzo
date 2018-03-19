35ee79cde06c244b95cd46c71119b3a3e86f4cd4
riccardo colombo <richi00@libero.it>
2018-03-19 09:12:52 +0100

    non vaaaaa

--- progetto_marzo/progetto_marzo/Poll.h
+++ progetto_marzo/progetto_marzo/Poll.h
@@ -6,44 +6,44 @@
 #include <vector>
 using namespace::std;
 
-class titoloPoll {
+class Poll {
 private:
 	char risp;
 	string dato;
 public:
+	Poll() {};
+	Poll(char a, string b) { risp = a; dato = b; };
 	std::vector<string> dati;
 	string nome;
-	void nomePoll();
+	void setRisp(char c) { risp = c; }
+	void setM(string d) { dato = d; }
+	char getRisp() { return risp; };
+	string getDato() { return dato; };
+
+	// Funzione che ti permette di dare un nome ad un nuovo poll
+	void nomePoll() {
+		cout << "\n Dai un nome al tuo nuovo poll ";
+		string in;
+		cin >> in;
+		nome = in;
+	};
+
+	// Funzione che ti permettere di inserire dati nel poll
+	void inserisci() {
+		do {
+			cout << "\n Inserisci il testo ";
+			cin >> dato;
+			dati.push_back(dato);
+			cout << "\n Vuoi inserire un altro dato? [S/N] ";
+			char risp;
+			cin >> risp;
+		} while (risp == 'S' || risp == 's');
+
 };
 
-// Funzione che ti permette di dare un nome ad un nuovo poll
-void creaPoll::nomePoll() {
-	cout << "\n Dai un nome al tuo nuovo poll ";
-	string in;
-	cin >> in;
-	nome = in;
-};
 
 
-class creaPoll : public titoloPoll {
-private:
-	char risp;
-	string dato;
-public:
-	void inserisci();
-	void nomePoll();
 
-};
-// Funzione che ti permettere di inserire dati nel poll
-void creaPoll::inserisci() {
-	do {
-		cout << "\n Inserisci il testo ";
-		cin >> dato;
-		dati.push_back(dato);
-		cout << "\n Vuoi inserire un altro dato? [S/N] ";
-		char risp;
-		cin >> risp;
-	} while (risp == 'S' || risp == 's');
 
 //class DefaultPoll {};
 // Funzione che ti permette di visualizzare il poll di default (poll di test)
