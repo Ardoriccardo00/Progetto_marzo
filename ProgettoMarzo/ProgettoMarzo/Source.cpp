acf52a2141c20f9fb905deb98c982f1c353d104b
PierPaolo Scampagna <lolpolpetta110@gmail.com>
2018-04-19 18:28:45 +0200

    dadada

--- ProgettoMarzo/ProgettoMarzo/Source.cpp
+++ ProgettoMarzo/ProgettoMarzo/Source.cpp
@@ -63,6 +63,8 @@ int main() {
 				cout << "\n Ciao Colombo, Piccolo Easter Egg";
 				cout << "\n Ciao Colombo, Piccolo Easter Egg";
 				cout << "\n Ciao Colombo, Piccolo Easter Egg";
+				cout << "\n Ciao Colombo, Piccolo Easter Egg";
+				
 				Poll p;
 				p.CreaPoll(login);
 
